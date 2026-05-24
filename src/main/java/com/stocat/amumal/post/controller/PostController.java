3d6af119090654701cf578860f69f617078c27f7
xEzIxX <by1094@gachon.ac.kr>
2026-05-24 22:57:37 +0900

    feat: 게시글 생성 컨트롤러 추가

---
+++ src/main/java/com/stocat/amumal/post/controller/PostController.java
@@ -0,0 +1,29 @@
+package com.stocat.amumal.post.controller;
+
+import com.stocat.amumal.common.response.ApiResponse;
+import com.stocat.amumal.post.dto.CreatePostRequest;
+import com.stocat.amumal.post.dto.CreatePostResponse;
+import com.stocat.amumal.post.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<CreatePostResponse> createPost(@RequestBody CreatePostRequest request) {
+        return ApiResponse.of("게시글이 생성되었습니다.", postService.createPost(request));
+    }
+}
