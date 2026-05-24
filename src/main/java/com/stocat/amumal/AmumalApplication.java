86428b93560cbde17c945da13ebae4711229f191
xEzIxX <by1094@gachon.ac.kr>
2026-05-24 21:51:36 +0900

    init: 서버 초기 세팅

---
+++ src/main/java/com/stocat/amumal/AmumalApplication.java
@@ -0,0 +1,13 @@
+package com.stocat.amumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmumalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmumalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stocat/amumal/AmumalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.stocat.amumal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmumalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
