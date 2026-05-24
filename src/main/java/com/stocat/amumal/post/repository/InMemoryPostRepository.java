549b881493ecdf70778a1013f1d8b45f683d28ee
xEzIxX <by1094@gachon.ac.kr>
2026-05-24 22:56:32 +0900

    feat: 게시글 저장소 로직 추가

---
+++ src/main/java/com/stocat/amumal/post/repository/InMemoryPostRepository.java
@@ -0,0 +1,25 @@
+package com.stocat.amumal.post.repository;
+
+import com.stocat.amumal.post.domain.Post;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class InMemoryPostRepository implements PostRepository {
+
+    private long sequence = 0;
+    private final Map<Long, Post> posts = new HashMap<>();
+
+    @Override
+    public Post save(Post post) {
+        // id값 전달 받으면 사용, 미전달 받으면 기존에 머지막으로 생성된 id값에서 +1한 값을 사용
+        long id = post.getId() == null ? ++sequence : post.getId();
+        // post 객체 생성
+        Post savedPost = new Post(id, post.getUserId(), post.getTitle(), post.getContent(), post.getImage());
+        // post 저장
+        posts.put(id, savedPost);
+        return savedPost;
+    }
+}
---
+++ src/main/java/com/stocat/amumal/post/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.stocat.amumal.post.repository;
+
+import com.stocat.amumal.post.domain.Post;
+
+public interface PostRepository {
+
+    Post save(Post post);
+}
