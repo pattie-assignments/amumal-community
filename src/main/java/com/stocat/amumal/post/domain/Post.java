ae183fc0cf2cccd546167821b00d8e39e43a2834
xEzIxX <by1094@gachon.ac.kr>
2026-05-24 23:17:47 +0900

    feat: 게시글 객체에 생성 시간, 조회수, 좋아요 수, 댓글수 필드 추가

--- src/main/java/com/stocat/amumal/post/domain/Post.java
+++ src/main/java/com/stocat/amumal/post/domain/Post.java
@@ -1,19 +1,29 @@
 package com.stocat.amumal.post.domain;
 
+import java.time.LocalDateTime;
+
 public class Post {
 
     private final Long id;
     private final Long userId;
+    private final LocalDateTime createdAt;
     private String title;
     private String content;
     private String image;
+    private int viewCount;
+    private int likeCount;
+    private int commentCount;
 
     public Post(Long id, Long userId, String title, String content, String image) {
         this.id = id;
         this.userId = userId;
+        this.createdAt = LocalDateTime.now();
         this.title = title;
         this.content = content;
         this.image = image;
+        this.viewCount = 0;
+        this.likeCount = 0;
+        this.commentCount = 0;
     }
 
     public Long getId() {
@@ -46,5 +56,25 @@ public class Post {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void increaseViewCount() {
+        this.viewCount++;
     }
 }
