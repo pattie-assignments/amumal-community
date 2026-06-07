191cacd2dd6f76d12e65f071065ddfb8ce23b40f
xEzIxX <by1094@gachon.ac.kr>
2026-06-07 21:22:43 +0900

    feat: PostLike 엔티티 추가

---
+++ src/main/java/com/stocat/amumal/post/domain/PostLike.java
@@ -0,0 +1,52 @@
+package com.stocat.amumal.post.domain;
+
+import com.stocat.amumal.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "post_like")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class PostLike {
+
+    // @EmbeddedId: 복합키 객체를 PK로 사용
+    @EmbeddedId
+    private PostLikeId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("postId") // PostLikeId.postId 값을 post.id로 자동으로 채워줌
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("userId")
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public static PostLike of(Post post, User user) {
+        PostLike postLike = new PostLike();
+        postLike.id = new PostLikeId(post.getId(), user.getId());
+        postLike.post = post;
+        postLike.user = user;
+        return postLike;
+    }
+}
---
+++ src/main/java/com/stocat/amumal/post/domain/PostLikeId.java
@@ -0,0 +1,24 @@
+package com.stocat.amumal.post.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable // 이 클래스가 다른 엔티티에 내장될 수 있음
+@EqualsAndHashCode // JPA가 동일성 비교 시 사용
+@NoArgsConstructor
+public class PostLikeId implements Serializable {
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    public PostLikeId(Long postId, Long userId) {
+        this.postId = postId;
+        this.userId = userId;
+    }
+}
