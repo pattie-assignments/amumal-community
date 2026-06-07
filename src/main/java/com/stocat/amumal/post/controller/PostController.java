f58e8aae529537d36af1f7977733008ba6282396
xEzIxX <by1094@gachon.ac.kr>
2026-06-07 23:22:45 +0900

    fix: 게시글 자원의 일부를 수정하므로 PUT → PATCH로 HTTP 메서드 수정

--- src/main/java/com/stocat/amumal/post/controller/PostController.java
+++ src/main/java/com/stocat/amumal/post/controller/PostController.java
@@ -8,11 +8,11 @@ import com.stocat.amumal.post.dto.GetPostsResponse;
 import com.stocat.amumal.post.dto.UpdatePostRequest;
 import com.stocat.amumal.post.dto.UpdatePostResponse;
 import com.stocat.amumal.post.service.PostService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,7 +53,7 @@ public class PostController {
         return ApiResponse.of("게시글 조회에 성공했습니다.", postService.getPost(postId, userId));
     }
 
-    @PutMapping("/{post_id}")
+    @PatchMapping("/{post_id}")
     @ResponseStatus(HttpStatus.OK)
     public ApiResponse<UpdatePostResponse> updatePost(
             @PathVariable("post_id") Long postId,
