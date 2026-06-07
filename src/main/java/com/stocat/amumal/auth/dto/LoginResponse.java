919717e04fec97464b9891692f9155b44b321afd
xEzIxX <by1094@gachon.ac.kr>
2026-06-07 22:46:12 +0900

    feat: LoginResponse 에 토큰 정보를 함께 응답하도록 수정

--- src/main/java/com/stocat/amumal/auth/dto/LoginResponse.java
+++ src/main/java/com/stocat/amumal/auth/dto/LoginResponse.java
@@ -3,7 +3,7 @@ package com.stocat.amumal.auth.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record LoginResponse(
-        @JsonProperty("user_id")
-        Long userId
+        @JsonProperty("user_id") Long userId,
+        TokenInfo token
 ) {
 }
