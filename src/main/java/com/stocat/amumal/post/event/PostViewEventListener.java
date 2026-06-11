2b91963be9d58f4081c51a1bb09392954da4592d
xEzIxX <by1094@gachon.ac.kr>
2026-06-11 18:11:20 +0900

    fix: 사용자가 게시글 조회 시 본인의 조회수를 포함하여 조회하도록 수정
    - 변경 전: 조회수 증가를 게시글 상세 조회 후 진행
    - 변경 후: 게시글 상세 조회 중 진행

--- src/main/java/com/stocat/amumal/post/event/PostViewEventListener.java
+++ src/main/java/com/stocat/amumal/post/event/PostViewEventListener.java
@@ -2,9 +2,8 @@ package com.stocat.amumal.post.event;
 
 import com.stocat.amumal.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
@@ -12,7 +11,7 @@ public class PostViewEventListener {
 
     private final PostService postService;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @EventListener
     public void handle(PostViewedEvent event) {
         postService.incrementViewCountCache(event.postId());
     }
