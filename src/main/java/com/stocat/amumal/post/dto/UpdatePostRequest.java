91f1d00768888a3c9ab1fd80f5385ee6c09dbb27
xEzIxX <by1094@gachon.ac.kr>
2026-06-14 19:09:10 +0900

    refactor: 게시글 수정 요청/응답 필드를 프론트 형식에 맞게 조정

--- src/main/java/com/stocat/amumal/post/dto/UpdatePostRequest.java
+++ src/main/java/com/stocat/amumal/post/dto/UpdatePostRequest.java
@@ -1,10 +1,12 @@
 package com.stocat.amumal.post.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdatePostRequest(
         @NotBlank String title,
         @NotBlank String content,
+        @JsonProperty("attachFileUrl")
         String image
 ) {
 }
