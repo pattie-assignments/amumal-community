32c04fd3d70b157b15a85334ce125c371f75ef5a
xEzIxX <by1094@gachon.ac.kr>
2026-06-14 20:04:51 +0900

    feat: 유저 유효성 응답 AuthCheckResponse 추가

---
+++ src/main/java/com/stocat/amumal/auth/dto/AuthCheckResponse.java
@@ -0,0 +1,11 @@
+package com.stocat.amumal.auth.dto;
+
+import com.stocat.amumal.user.dto.UserResponse;
+
+public record AuthCheckResponse(
+        String code,
+        String message,
+        UserResponse data,
+        Long idx
+) {
+}
