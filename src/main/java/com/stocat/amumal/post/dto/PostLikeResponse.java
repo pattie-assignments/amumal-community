e23c9d8fc62ee1e8026a1e73b02f79b6c12e1f9f
xEzIxX <by1094@gachon.ac.kr>
2026-06-14 19:10:06 +0900

    refactor: 게시글 좋아요 응답 필드를 프론트 형식에 맞게 조정

--- src/main/java/com/stocat/amumal/post/dto/PostLikeResponse.java
+++ src/main/java/com/stocat/amumal/post/dto/PostLikeResponse.java
@@ -1,11 +1,7 @@
 package com.stocat.amumal.post.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record PostLikeResponse(
-        @JsonProperty("post_id")
         Long postId,
-        @JsonProperty("like_count")
         int likeCount
 ) {
 }
