116d832b4801b2b8a091597dd2079e3be94e10a0
xEzIxX <by1094@gachon.ac.kr>
2026-06-15 11:49:33 +0900

    feat: 게시글 이미지 매핑 서비스 로직 추가

--- src/main/java/com/stocat/amumal/image/repository/ImageRepository.java
+++ src/main/java/com/stocat/amumal/image/repository/ImageRepository.java
@@ -1,7 +1,9 @@
 package com.stocat.amumal.image.repository;
 
 import com.stocat.amumal.image.domain.Image;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
+    Optional<Image> findByFileUrl(String fileUrl);
 }
---
+++ src/main/java/com/stocat/amumal/post/repository/PostImageRepository.java
@@ -0,0 +1,11 @@
+package com.stocat.amumal.post.repository;
+
+import com.stocat.amumal.post.domain.PostImage;
+import com.stocat.amumal.post.domain.PostImageId;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, PostImageId> {
+    List<PostImage> findById_PostId(Long postId);
+    void deleteAllById_PostId(Long postId);
+}
